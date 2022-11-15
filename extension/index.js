544e852818acd9488e89a60d294579607daa373b
Elton Tanaka <elton.tanaka@univem.edu.br>
2022-11-15 10:38:08 -0300

    added bundle

---
+++ extension/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function (nodecg) {
+	nodecg.Replicant('textReplicant', {defaultValue: 'Welcome to the Stream!'}),
+	nodecg.Replicant('activeFacecam', {defaultValue: 'data:,'}),
+	nodecg.Replicant('facecamTimer', {defaultValue: '300000'})
+};
